69b017aa0dda649d1eb44c158bacb28013528cbd
smriti <sonkar.smriti31@gmail.com>
2024-05-03 14:33:55 +0530

    2 files added

---
+++ conditional_progs/grades.py
@@ -0,0 +1,18 @@
+a = int(input("Enter your marks:  "))
+
+if(a>=90 ):
+    print("your grade is A")
+    
+elif( 80<=a<90):
+    print("your grade is B")
+
+elif( 70<=a<80):
+    print("your grade is C")
+    
+elif( 50<=a<70):
+    print("your grade is D")
+    
+elif( 40<=a<50):    
+    print("your grade is E")
+else :
+    print("FAil")
---
+++ for_progs/table.py
@@ -0,0 +1,8 @@
+# Python program to print a multiplication table of a given number
+
+number = int (input("\nEnter a number for table :"))
+
+for i in range (1, 11) :
+   
+    print (number," x",i," =",number*i)
+    
