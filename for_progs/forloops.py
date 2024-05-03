8763556397dd18d469f156d757c81b641e81c994
smriti <sonkar.smriti31@gmail.com>
2024-05-03 12:49:05 +0530

    changed path

---
+++ for_progs/forloops.py
@@ -0,0 +1,21 @@
+# for(start_value_included, last_value_excluded)
+for i in range(1, 6) :
+    print(i)
+
+print('\nNew task....')
+
+# for(start_value_included, last_value_excluded, step_value)
+for j in range(1,10,3) :
+    print(j)
+print("\n")
+
+#for loop in string vertically
+a = "sam"
+for i in a :
+    print(i)
+    
+#for loop in string horizontally
+print("\n")
+a = "sam"
+for i in a :
+    print(i, end=" ")
