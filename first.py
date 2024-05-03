7b23520f9674d6417451b45c23742b816ba3b03a
smriti <sonkar.smriti31@gmail.com>
2024-05-03 12:12:26 +0530

    added 2 files

---
+++ first.py
@@ -0,0 +1,11 @@
+a = int(input("Enter your age"))
+
+if(0<=a<=20):
+    print("child")
+    
+elif(21<=a<=50):
+    print("Young")
+elif(51<=a<=80):
+    print("Old")
+else :
+    print("Try again")
---
+++ menu.py
@@ -0,0 +1,19 @@
+a = input("Enter your yummy item:  ")
+
+if(a == "Pizza"):
+    print("your item is Pizza and its price is Rs. 200")
+    
+elif(a == "burgur"):
+    print("your item is burgur and its price is Rs. 150")
+
+elif(a == "sandwich"):
+    print("your item is sandwich and its price is Rs. 100")
+    
+elif(a == "biryani"):
+    print("your item is biryani and its price is Rs. 300")
+    
+elif(a == "panipuri"):
+    print("your item is panipuri and its price is Rs. 20")
+
+else :
+    print("OOPs..Item not found")
