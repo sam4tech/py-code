9cae75aa197c9c257918132e1450989dbba52a21
smriti <sonkar.smriti31@gmail.com>
2024-05-05 03:42:46 +0530

    2 files added

---
+++ conditional_progs/calculator.py
@@ -0,0 +1,23 @@
+# Calculation of numbers
+
+choice = input("Enter your choice : ")
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+
+if (choice == "addition"):
+    result = num1 + num2
+    print(result)
+
+
+
+elif (choice == "subtraction") :
+    result = num1 - num2
+    print(result)
+
+
+elif (choice == "multiplication") :
+    result = num1 * num2
+    print(result)
+
+else :
+    print("Try again...")
---
+++ conditional_progs/maximum.py
@@ -0,0 +1,13 @@
+# Maximum of two numbers
+
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
+
+if (a>b):
+   print("a is maximum")
+
+elif (b>a) :
+    print("b is maximum")
+
+else :
+    print("a is equal to b")
