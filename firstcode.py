47323a9d4f524a25a2f28fe9c03f9520a08e1bba
smriti <sonkar.smriti31@gmail.com>
2024-05-07 17:32:00 +0530

    basics for python added

---
+++ firstcode.py
@@ -0,0 +1,36 @@
+print("Hello world", end ="--")
+print("hii sam")
+
+# # print separate words
+print("good", "morning") 
+
+# variables
+age = 10
+print(type(age))
+
+city = "Jaipur"
+print(type(city))
+
+
+address="""malviya nagar
+Jaipur"""
+print(address)
+
+company = "regex"
+year = 2024
+print("company name is", company, "Year is", year)
+print(f"company name is {company} Year is {year}")
+
+#String format method
+
+username ="aman"
+msg = f"hello user {username}"
+
+print(msg)
+username = "tushar"
+print(msg)
+
+# .format method
+
+print("Company name is {} and year is {}".format("regex", 2024))
+print("Company name is {0} and year is {1}".format("regex", 2024))
