b9d7894f60458ab0be183ae744c14d4797bc3764
smriti <sonkar.smriti31@gmail.com>
2024-05-11 13:43:26 +0530

    file added

---
+++ strings/divisible_5_7.py
@@ -0,0 +1,8 @@
+# Make a loop from 99 to 4 and print numbers that are divisible by 5 and 7
+
+for i in range (99, 3, -1) :
+    # print(i)
+    if (i%5 == 0 and i%7 == 0) :
+        print(i, end = " ")
+    # else :
+    #     print("", end = " ")
