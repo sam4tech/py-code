2f562a48fd2eb23abf176df53b1d84f8e49135fd
smriti <sonkar.smriti31@gmail.com>
2024-05-17 12:29:21 +0530

    code updated

--- Practice/armstrong.py
+++ Practice/armstrong.py
@@ -1,15 +1,55 @@
 # Q5 Write a program that will check whether the number is armstrong number or not.
 
-num = input("Enter a number : ")
-# temp = num
+num = int(input("Enter the number : \n >>> "))
+temp = num
 sum = 0
 
-for i in num :
-    c = int(i) ** 3
-    sum = sum + c 
-print (f"Cube of digits : {sum}")
+while num > 0 :
+    sum = sum  +((num % 10)**3)
+    num = num//10
 
-if sum == int(num)  :
-    print("Armstrong number")
+print("original number : ", temp)
+print("Sum of numbers : ", sum)
+
+
+if temp == sum :
+    print("Number is Armstrong.")
 else :
-    print("Not armstrong.")
+    print("Number is NOT Armstrong.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# num = input("Enter a number : ")
+# # temp = num
+# sum = 0
+
+# for i in num :
+#     c = int(i) ** 3
+#     sum = sum + c 
+# print (f"Cube of digits : {sum}")
+
+# if sum == int(num)  :
+#     print("Armstrong number")
+# else :
+#     print("Not armstrong.")
--- Practice/narcissist_number.py
+++ Practice/narcissist_number.py
@@ -1,19 +1,51 @@
 
-num = input("Enter a number : ")
-# temp = num
-sum = 0
-cnt = 0
-length = len(num)
-for i in num :
-    c = int(i) ** length
-    sum = sum + c 
-    cnt +=1 
-print (f"sum of power of digits : {sum}")
-print(cnt)
-print(len(num))
 
-if cnt == length and sum == int(num)  :
-        print("Narcissist number")
+num = int(input("Enter the number : \n >>> "))
+# num = 1634
+temp = num
+sum = 0
+print(len(str(temp)))
+while num > 0 :
+    sum = sum  +((num % 10)**len(str(temp)))
+    num = num//10
+
+print("original number : ", temp)
+print("Sum of numbers : ", sum)
+
+
+if temp == sum :
+    print("Number is Narcissist.")
 else :
-        print("Not Narcissist.")
+    print("Number is NOT Narcissist.")
+
+
+
+
+
+
+
+
+
+
+
+# num = input("Enter a number : ")
+# # temp = num
+# sum = 0
+# cnt = 0
+# length = len(num)
+# for i in num :
+#     c = int(i) ** length
+#     sum = sum + c 
+#     cnt +=1 
+# print (f"sum of power of digits : {sum}")
+# print(cnt)
+# print(len(num))
+
+# if cnt == length and sum == int(num)  :
+#         print("Narcissist number")
+# else :
+#         print("Not Narcissist.")
+
+
+
 
