c58a0810ca74d826e4658325f123081cb8e85d71
smriti <sonkar.smriti31@gmail.com>
2024-05-23 20:50:50 +0530

    new file added

---
+++ Practice2/negative_nos.py
@@ -0,0 +1,10 @@
+# Display numbers from -10 to -1 using for loop
+
+
+print ("Negative numbers from -10 to -1 :")
+for i in range (-10, 0) :
+    print(i, end =" ")
+
+#output
+# Negative numbers from -10 to -1 :
+# -10 -9 -8 -7 -6 -5 -4 -3 -2 -1 
