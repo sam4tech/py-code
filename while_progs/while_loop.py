6f4581cbb8ccbc339504e4ef4c5c57271856ed4d
smriti <sonkar.smriti31@gmail.com>
2024-05-26 23:57:55 +0530

    changes done

---
+++ while_progs/while_loop.py
@@ -0,0 +1,86 @@
+i = 0
+while(i<10) :
+    print(i, end =" ")
+    i+=1
+# 0 1 2 3 4 5 6 7 8 9 
+
+
+print("a=============")
+i = 0
+while(i<10) :
+    i+=1
+    print(i, end =" ")
+
+
+# 1 2 3 4 5 6 7 8 9 10
+
+
+
+print("b=============")
+
+
+
+i = 5
+while(i<10) :
+    i+=1
+    print(i, end =" ")
+
+# 6 7 8 9 10
+
+print("c=============")
+
+# i = 5
+# while(True) :
+#     i+=1
+#     # print(i, end =" ")
+
+
+
+print("=============aa")
+
+# i = 5
+# while(True) :
+#     print(i)
+# #5
+
+
+print("+++++++++++++++++++++++++++")
+
+i = 10
+while(i<11):
+    if  i==10 :
+        print(i)
+        break
+    i+=1
+
+print("prints first 5 even nos+++++++++++++++++++++++++++")
+
+count=0
+tot_num = 0
+while (True) :
+    if(count%2==0) :
+        print(count)
+        tot_num +=1
+    if(tot_num == 5) :
+        break
+    count+=1
+
+
+
+j = 5
+while(True) :
+    if (j==10) :
+        print(j, end =" ")
+        break
+    j+=1
+
+
+print("====================")
+k = 0
+cnt = 0
+while (true) :
+    if k%2 == 0 :
+        print(k)
+        cnt+=1
+        break
+    k+=1
