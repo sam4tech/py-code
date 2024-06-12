0c90cc2f1980a8c79a50f4c0ab922482a2a19a1f
smriti <sonkar.smriti31@gmail.com>
2024-06-12 17:43:55 +0530

    string changed

--- lists/dictionary.py
+++ lists/dictionary.py
@@ -22,7 +22,7 @@ print(c)
 ######
 
 
-string = "hey hello tushar"
+string = "hey hello good morning"
 # cnt = 0
 dict = { 'char': 0}
 for char in string :
@@ -35,11 +35,26 @@ for char in string :
             dict[char] = dict[char] + 1
 
 print(dict)
+
+
+
+
+
+
+
 print("==================================")
 #write a program in python to calculate savings as:
 #a person save 1rs on monday, 2 on tuesday, 3 on wednesday, 4 on thursday, 5 on friday and 6 on saturday and 7 on sunday.
 # next week day savings  starts from 2 rs on Monday, and so on.
 # calculate the total savings as per the days 
+
+
+
+
+
+
+
+
 
 def calculate_total_savings(num_weeks):
     total_savings = 0
@@ -52,9 +67,9 @@ def calculate_total_savings(num_weeks):
                 day_savings = 1  # Reset day savings to 1 for Monday
     return total_savings
 
-num_weeks = int(input("Enter the number of weeks: "))
+#num_weeks = int(input("Enter the number of weeks: "))
 total = calculate_total_savings(num_weeks)
-print("Total savings over {} weeks: {}".format(num_weeks, total))
+#print("Total savings over {} weeks: {}".format(num_weeks, total))
 
 
 
